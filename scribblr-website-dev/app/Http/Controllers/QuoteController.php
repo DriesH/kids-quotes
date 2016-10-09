984f976e4e6a11b53e07e0e802817377fd2726a3
jorenvh1 <joren.vh@hotmail.com>
2016-10-09 18:37:57 +0200

    *Note to self*: NEVER EVER try to upload images with an ajax call, you'll probably commit suicide next time

--- scribblr-website-dev/app/Http/Controllers/QuoteController.php
+++ scribblr-website-dev/app/Http/Controllers/QuoteController.php
@@ -30,49 +30,31 @@ class QuoteController extends Controller
     public function newQuote (Request $request) {
         $quote        = $request->json('quote');
         $child_id     = $request->json('child_id');
+        $backgr_img   = $request->file("userfile");
+        if($backgr_img){
+              /**
+              *de extensie van afbeelding
+              *
+              *@var string
+              */
+              $ext = $backgr_img->getClientOriginalExtension();
 
-        if($request->json('backgr_img')){
-            dd("img was found");
-        }
-    //     if(Input::hasFile('foto')){
-    //         /**
-    //         *afbeelding is rauwe input data van de aflbeeding
-    //         *
-    //         *@var file
-    //         */
-    //         $maxWidth = 1000;
-    //         $maxHeight = 1000;
-    //         $afbeelding = Input::file('foto');
-    //         $imageDimensions = getimagesize($afbeelding);
-    //         if ($imageDimensions['0'] <= $maxWidth && $imageDimensions['1'] <= $maxHeight ) {
-    //           /**
-    //           *de extensie van afbeelding
-    //           *
-    //           *@var string
-    //           */
-    //           $extensie = $afbeelding->getClientOriginalExtension();
-    //           /**
-    //           *nieuwe unieke naam van de afbeelding
-    //           *
-    //           *@var string
-    //           */
-    //           $nieuwe_naam = uniqid() . "." . $extensie;
-    //           $afbeelding->move('pictures/uploads', $nieuwe_naam);
-    //           /**
-    //           *nieuw pad naar afbeelding
-    //           *
-    //           *@var string
-    //           */
-    //           $foto_path = '/pictures/uploads/' . $nieuwe_naam;
-    //           //dd($foto_path);
-    //       }
-    //   }
+              /**
+              *nieuwe unieke naam van de afbeelding
+              *
+              *@var string
+              */
+              $nieuwe_naam = uniqid() . "." . $ext;
+              $backgr_img->move('pictures', $nieuwe_naam);
+      }
 
         $newQuote = Quote::create([
             'quote' => $quote,
             'child_id' => $child_id,
             'backgr_img' => 'test',
         ]);
+
+        //aanpassen!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
         return $newQuote;
