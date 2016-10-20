ae7754d6db7c957b466676afc52a7815d4d52bfe
Dries Heyninck <dries.heyninck@gmail.com>
2016-10-20 23:46:10 +0200

    update

--- scribblr-website-dev/resources/assets/js/components/partial/BodyDashboardPersonal.vue
+++ scribblr-website-dev/resources/assets/js/components/partial/BodyDashboardPersonal.vue
@@ -1,10 +1,68 @@
 <template lang="html">
-    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main push">
+    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         <div class="row placeholders">
             <div id="grid" data-columns>
-                <div class="thumbnail" v-for='child in currentChildren'>
+                <div class="thumbnail">
                     <img src="/business_imgs/elf.jpg" />
                 </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
+                </div>
+                
             </div>
         </div>
     </div>
