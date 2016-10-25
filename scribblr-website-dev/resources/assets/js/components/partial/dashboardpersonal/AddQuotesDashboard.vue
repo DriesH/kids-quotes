d35debbb9e07ceba67a534b617516c90d86d6730
Dries Heyninck <dries.heyninck@gmail.com>
2016-10-25 20:42:54 +0200

    test

--- scribblr-website-dev/resources/assets/js/components/partial/dashboardpersonal/AddQuotesDashboard.vue
+++ scribblr-website-dev/resources/assets/js/components/partial/dashboardpersonal/AddQuotesDashboard.vue
@@ -1,22 +1,6 @@
 <template lang="html">
     <div class="col-md-8 col-sm-8 sidebar">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <button type="button" class="btn btn-danger pull-right" name="hide" @click="hidePanel"><i class="fa fa-ban"></i> Hide</button>
-                </div>
-            </div>
-            <div class="row" v-if="addingQuote">
-                <form>
-                    <!-- NAME START -->
-                    <div class="form-group">
-                        <label for="childName">Full name: </label>
-                        <input type="text" class="form-control" id="childName" placeholder="Fullname" v-model="newChild.childName" :class="{ 'error-form' : errorMessagesForm.childName }">
-                    </div>
-                    <!-- NAME END -->
-                </form>
-            </div>
-        </div>
+        <button type="button" class="btn btn-danger" name="hide" @click="hidePanel"><i class="fa fa-ban"></i> Hide</button>
     </div>
 </template>
 
@@ -27,7 +11,7 @@
         ],
         data () {
             return {
-                addingQuote: false
+
             }
         },
         computed: {
--- scribblr-website-dev/resources/assets/js/components/partial/dashboardpersonal/BodyDashboardPersonal.vue
+++ scribblr-website-dev/resources/assets/js/components/partial/dashboardpersonal/BodyDashboardPersonal.vue
@@ -1,26 +1,12 @@
 <template lang="html">
     <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         <div class="row placeholders">
-
-            <!-- ADD QUOTE BTN START -->
-            <div class="add-btn col-md-12 col-xs-12 col-lg-12">
-                <button type="button" name="button" class="btn btn-success" @click="openAddQuoteForm"><i class="fa fa-plus"></i>  ADD NEW QUOTE</button>
-            </div>
-            <!-- ADD QUOTE BTN END -->
-
-            <!-- GRID WITH QUOTES START -->
             <div id="grid" data-columns>
-                <div class="thumbnail quotes">
-                    <img src="/business_imgs/apple.jpg" alt="" />
+                <div class="thumbnail">
+                    <img src="/business_imgs/elf.jpg" />
                 </div>
             </div>
-            <!-- GRID WITH QUOTES END -->
-
         </div>
-
-        <!-- ADDQUOTES START -->
-        <add-quotes-dashboard v-if="addQuoteShow" style="z-index: 100;" v-bind:add-quote-show.sync="addQuoteShow" :class="{ 'overlay-sidebar-shadow': addQuoteShow }"></add-quotes-dashboard>
-        <!-- ADDQUOTES START -->
     </div>
 </template>
 
@@ -28,32 +14,15 @@
     export default {
         data () {
             return {
-                addQuoteShow: false //show - hide form add quote
+                
             }
         },
         computed: {},
         ready () {},
-        methods: {
-            openAddQuoteForm: function () {
-                this.addQuoteShow = true;
-            },
-            getPreviousQuotes: function () {
-                //get previous quotes from current child
-                this.$http.get('/api/quote/' + id).then((success_response) => {
-                    this.previousQuotes = JSON.parse(success_response.body);
-                },
-                (error_response) => {
-                    alert('error');
-                });
-            }
-        },
+        methods: {},
         components: {}
     }
 </script>
 
-<style lang="css" scoped>
-    .add-btn {
-        padding: 5px;
-        margin-bottom: 21px;
-    }
+<style lang="css">
 </style>
