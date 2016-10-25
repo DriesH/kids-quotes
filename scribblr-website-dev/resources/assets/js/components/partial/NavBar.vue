addb9a5f608f7a2a0093f311f9d2b2b26b64ca05
Dries Heyninck <dries.heyninck@gmail.com>
2016-10-25 20:56:37 +0200

    update

--- scribblr-website-dev/resources/assets/js/components/partial/NavBar.vue
+++ scribblr-website-dev/resources/assets/js/components/partial/NavBar.vue
@@ -16,7 +16,7 @@
                     <li v-if="!data.user.isLoggedIn"><a href="/login">Log In</a></li>
                     <li v-if="!data.user.isLoggedIn"><a href="/register">Sign up</a></li>
 
-                    <li v-if="data.user.isLoggedIn"><a href="/dashboard">{{ data.user.name }}</a></li>
+                    <li v-if="data.user.isLoggedIn"><a href="/dashboard">{{ data.user.name }} <i class="fa fa-user"></i></a></li>
                     <li v-if="data.user.isLoggedIn"><a href="/logout">Logout</a></li>
                 </ul>
             </div>
