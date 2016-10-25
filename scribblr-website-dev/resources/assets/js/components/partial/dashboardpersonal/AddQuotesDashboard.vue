144e2af73b18c92eb2d03fcf75e90a0593e53369
Dries Heyninck <dries.heyninck@gmail.com>
2016-10-25 20:51:37 +0200

    update

--- scribblr-website-dev/resources/assets/js/components/partial/dashboardpersonal/AddQuotesDashboard.vue
+++ scribblr-website-dev/resources/assets/js/components/partial/dashboardpersonal/AddQuotesDashboard.vue
@@ -1,6 +1,22 @@
 <template lang="html">
     <div class="col-md-8 col-sm-8 sidebar">
-        <button type="button" class="btn btn-danger" name="hide" @click="hidePanel"><i class="fa fa-ban"></i> Hide</button>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <button type="button" class="btn btn-danger pull-right" name="hide" @click="hidePanel"><i class="fa fa-ban"></i> Hide</button>
+                </div>
+            </div>
+            <div class="row" v-if="addingQuote">
+                <form>
+                    <!-- NAME START -->
+                    <div class="form-group">
+                        <label for="childName">Full name: </label>
+                        <input type="text" class="form-control" id="childName" placeholder="Fullname" v-model="newChild.childName" :class="{ 'error-form' : errorMessagesForm.childName }">
+                    </div>
+                    <!-- NAME END -->
+                </form>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,7 +27,7 @@
         ],
         data () {
             return {
-
+                addingQuote: false
             }
         },
         computed: {
