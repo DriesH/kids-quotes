ab08e17aba39f9cd0b3060ab9de19c1e8e32af21
DriesH <dries.heyninck@gmail.com>
2016-10-27 13:08:40 +0200

    update

--- scribblr-website-dev/resources/assets/js/components/partial/dashboardpersonal/AddQuotesDashboard.vue
+++ scribblr-website-dev/resources/assets/js/components/partial/dashboardpersonal/AddQuotesDashboard.vue
@@ -8,9 +8,20 @@
                     <input type="text" class="form-control" id="quoteName" placeholder="Your quote..." v-model="quote.quoteName">
                 </div>
 
+                <div class="cc-selector-2">
+                    <div v-for="defaultImg in defaultImgs">
+                        <input id="{{defaultImg}}" type="radio" name="background" value="{{defaultImgs.indexOf(defaultImg)}}" v-model="backgroundChosen"/>
+                        <label class="drinkcard-cc" for="{{defaultImg}}" v-bind:style="{ backgroundImage : 'url(' + prefixDefault + defaultImg + ')' }"></label>
+                    </div>
+                    <div>
+                        <input id="customBackground" type="radio" name="background" value="custom" v-model="backgroundChosen"/>
+                        <label class="drinkcard-cc" for="customBackground"></label>
+                    </div>
+                </div>
+
                 <button type="button" name="add" class="btn btn-success"><i class="fa fa-plus"></i> Add Quote</button>
 
-                <button type="button" class="btn btn-danger pull-right" name="hide" @click="hidePanel"><i class="fa fa-ban"></i> Hide</button>
+                <button type="button" class="btn btn-danger pull-right" name="hide" @click="closeAddQuoteForm"><i class="fa fa-ban"></i> Hide</button>
 
 
 
@@ -28,8 +39,10 @@
         data () {
             return {
                 quote: {
-                    quoteName: ''
-                }
+                    quoteName: '',
+                    backgrImg: ''
+                },
+                defaultImgs: ['wood.jpg', 'chalkboard.jpg', 'paper.jpg']
             }
         },
         computed: {
@@ -39,9 +52,16 @@
 
         },
         methods: {
-            hidePanel: function () {
+            closeAddQuoteForm: function () {
                 this.addQuoteShow = false;
+                var sideBar = document.getElementById('sidebar-div');
+                if(this.hasClass(sideBar, 'overlay-sidebar-shadow')) {
+                    sideBar.className = 'col-sm-3 col-md-2 sidebar';
+                }
             },
+            hasClass: function (element, cls) {
+                return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+            }
         },
         components: {
 
@@ -50,4 +70,32 @@
 </script>
 
 <style lang="css" scoped>
+    .cc-selector-2 input{
+        position:absolute;
+        z-index:999;
+    }
+    .cc-selector-2 input:active +.drinkcard-cc, .cc-selector input:active +.drinkcard-cc{opacity: .9;}
+    .cc-selector-2 input:checked +.drinkcard-cc, .cc-selector input:checked +.drinkcard-cc{
+        -webkit-filter: none;
+           -moz-filter: none;
+                filter: none;
+    }
+    .drinkcard-cc{
+        cursor:pointer;
+        background-size:contain;
+        background-repeat:no-repeat;
+        display:inline-block;
+        width:100px;height:70px;
+        -webkit-transition: all 100ms ease-in;
+           -moz-transition: all 100ms ease-in;
+                transition: all 100ms ease-in;
+        -webkit-filter: brightness(1.8) grayscale(1) opacity(.7);
+           -moz-filter: brightness(1.8) grayscale(1) opacity(.7);
+                filter: brightness(1.8) grayscale(1) opacity(.7);
+    }
+    .drinkcard-cc:hover{
+        -webkit-filter: brightness(1.2) grayscale(.5) opacity(.9);
+           -moz-filter: brightness(1.2) grayscale(.5) opacity(.9);
+                filter: brightness(1.2) grayscale(.5) opacity(.9);
+    }
 </style>
