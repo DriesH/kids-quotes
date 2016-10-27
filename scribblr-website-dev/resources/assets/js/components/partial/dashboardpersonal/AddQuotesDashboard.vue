e24d196027f240945c951b2826c68504e0074d25
DriesH <dries.heyninck@gmail.com>
2016-10-27 11:54:13 +0200

    update

--- scribblr-website-dev/resources/assets/js/components/partial/dashboardpersonal/AddQuotesDashboard.vue
+++ scribblr-website-dev/resources/assets/js/components/partial/dashboardpersonal/AddQuotesDashboard.vue
@@ -1,21 +1,15 @@
 <template lang="html">
     <div class="col-md-8 col-sm-8 sidebar">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <button type="button" class="btn btn-danger pull-right" name="hide" @click="hidePanel"><i class="fa fa-ban"></i> Hide</button>
+            <button type="button" class="btn btn-danger pull-right" name="hide" @click="hidePanel"><i class="fa fa-ban"></i> Hide</button>
+            
+            <form>
+                <!-- NAME START -->
+                <div class="form-group">
+                    <label for="quoteName">Quote: </label>
+                    <input type="text" class="form-control" id="quoteName" placeholder="Fullname" v-model="newChild.childName">
                 </div>
-            </div>
-            <div class="row" v-if="addingQuote">
-                <form>
-                    <!-- NAME START -->
-                    <div class="form-group">
-                        <label for="childName">Full name: </label>
-                        <input type="text" class="form-control" id="childName" placeholder="Fullname" v-model="newChild.childName" :class="{ 'error-form' : errorMessagesForm.childName }">
-                    </div>
-                    <!-- NAME END -->
-                </form>
-            </div>
+                <!-- NAME END -->
+            </form>
         </div>
     </div>
 </template>
